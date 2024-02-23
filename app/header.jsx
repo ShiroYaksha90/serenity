61aecd6b4fc9e8859361652bf644474c162fe415
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-02-23 20:22:33 +0300

    Update header links

--- app/header.jsx
+++ app/header.jsx
@@ -1,23 +1,17 @@
+import Link from "next/link";
+
 const Header = () => {
     return (
         <div className="navbar bg-base-100">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <Link href="/" className="btn btn-ghost text-xl">Serenity</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>
-            Parent
-          </summary>
-          <ul className="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
+      <li><Link href="/about">About</Link></li>
+      <li><Link href="/about/contact">Contact</Link></li>
+      <li><Link href="/githubusers">GitHub Users</Link></li>
+      
     </ul>
   </div>
 </div>
