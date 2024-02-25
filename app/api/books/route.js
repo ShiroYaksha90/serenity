a8c84cd518455b00478388ba46a9aa7e3bc0b9ef
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-02-25 23:23:24 +0300

    Add POST endpoint to create a new book

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -1,6 +1,19 @@
 import books from './data.json';
 import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
 
 export const GET = async (req) => {
     return NextResponse.json(books);
+}
+
+export const POST = async (req) => {
+    const { title, link, img } = await req.json();
+    const newBook = {
+        id: uuidv4(),
+        title,
+        link,
+        img
+    };
+    books.push(newBook);
+    return NextResponse.json('Book added successfully!');
 }
