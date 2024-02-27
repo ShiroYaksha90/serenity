e0c55663b2b6515a160000116bf42a9b3258cf34
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-02-27 23:55:00 +0300

    Refactor header component styling

--- app/header.jsx
+++ app/header.jsx
@@ -1,21 +1,28 @@
 import Link from "next/link";
 
 const Header = () => {
-    return (
-        <div className="navbar bg-indigo-500">
-  <div className="flex-1">
-    <Link href="/" className="btn btn-ghost text-xl">Serenity</Link>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><Link href="/about">About</Link></li>
-      <li><Link href="/about/contact">Contact</Link></li>
-      <li><Link href="/githubusers">GitHub Users</Link></li>
-      
-    </ul>
-  </div>
-</div>
-    )
-}
+  return (
+    <div className="navbar bg-blue-50 text-amber-950">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-xl">
+          Serenity
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/about/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/githubusers">GitHub Users</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
