00f2281e827cd3861ab8cb0b8910671b1d509c31
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-02-28 00:48:03 +0300

    Refactor AddBook component

--- app/components/AddBook.jsx
+++ app/components/AddBook.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const AddBook = ({refreshBooks}) => {
+const AddBook = ({ refreshBooks }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [newBookTitle, setNewBookTitle] = useState("");
   const [newBookLink, setNewBookLink] = useState("");
@@ -10,27 +10,30 @@ const AddBook = ({refreshBooks}) => {
 
   const handleSubmitNewBook = async (e) => {
     e.preventDefault();
-    const res = await fetch(`/api/books/`,{
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            title: newBookTitle,
-            link: newBookLink,
-            img: newBookImg
-        })
-    })
-    if(res.ok){
-        setNewBookTitle("");
-        setModalOpen(false);
-        refreshBooks();
+    const res = await fetch(`/api/books/`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        title: newBookTitle,
+        link: newBookLink,
+        img: newBookImg,
+      }),
+    });
+    if (res.ok) {
+      setNewBookTitle("");
+      setModalOpen(false);
+      refreshBooks();
     }
   };
 
   return (
     <div>
-      <button className="btn btn-outline btn-primary mb-3" onClick={() => setModalOpen(true)}>
+      <button
+        className="btn bg-blue-100 border-amber-950 text-amber-950 mb-3 hover:bg-blue-200 hover:text-rose-50 "
+        onClick={() => setModalOpen(true)}
+      >
         Add Book
       </button>
       <dialog
@@ -39,16 +42,16 @@ const AddBook = ({refreshBooks}) => {
       >
         <form
           method="dialog"
-          className="modal-box"
+          className="modal-box bg-rose-100 shadow-2xl p-4"
           onSubmit={handleSubmitNewBook}
         >
           <button
-            className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2"
+            className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2 text-amber-950"
             onClick={() => setModalOpen(false)}
           >
             x
           </button>
-          <h3 className="text-2xl font-bold">Add Book</h3>
+          <h3 className="text-2xl font-bold text-amber-950 mb-3">Add Book</h3>
           <input
             type="text"
             value={newBookTitle}
@@ -61,7 +64,7 @@ const AddBook = ({refreshBooks}) => {
             value={newBookLink}
             onChange={(e) => setNewBookLink(e.target.value)}
             placeholder="Enter New Book Link"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full max-w-xs my-1"
           />
           <input
             type="text"
@@ -70,7 +73,10 @@ const AddBook = ({refreshBooks}) => {
             placeholder="Enter New Book Image URL"
             className="input input-bordered w-full max-w-xs"
           />
-          <button className="btn btn-primary" type="submit">
+          <button
+            className="btn bg-blue-100 border-amber-900 text-amber-950 mx-1 hover:bg-blue-200 hover:text-rose-50"
+            type="submit"
+          >
             Add Book
           </button>
         </form>
