a850caaa393488a8641e8e32d19bbe0fb304d245
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-02-28 01:50:05 +0300

    Refactor book API route to handle errors

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -2,18 +2,28 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../db";
 
 export const GET = async (req) => {
-  const books = await prisma.book.findMany();
-  return NextResponse.json(books);
-};
+    try {
+        const books = await prisma.book.findMany();
+        return NextResponse.json(books);
+      } catch (error) {
+        console.error("Error fetching books: ", error);
+        return NextResponse.json({ error: "Error fetching books" });
+      }
+    };
 
 export const POST = async (req) => {
   const { title, link, img } = await req.json();
-  await prisma.book.create({
-    data: {
-      title: title,
-      link: link,
-      img: img,
-    },
-  });
-  return NextResponse.json("Book added successfully!");
+  try {
+    await prisma.book.create({
+      data: {
+        title: title,
+        link: link,
+        img: img,
+      },
+    });
+    return NextResponse.json("Book added successfully!");
+  } catch (error) {
+    console.error("Error adding book: ", error);
+    return NextResponse.json({ error: "Error adding book" });
+  }
 };
