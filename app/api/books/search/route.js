11f3a77d75943aebd90e73bc97360e8a30c30ebc
ShiroYaksha90 <aljazaeribasim@gmail.com>
2024-03-05 00:30:02 +0300

    Update prisma import path in book search route

--- app/api/books/search/route.js
+++ app/api/books/search/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../db";
+import { prisma } from '../../../../lib/prisma'
 
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
